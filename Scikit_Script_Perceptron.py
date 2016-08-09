d598d141d3581d1e420fc80d9f368ee2d304e3da
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2016-08-09 18:30:53 -0400

    Perceptron in scikit

---
+++ Scikit_Script_Perceptron.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+In this script, I play around with the Perceptron algorithm and splitting data 
+sets into training and test subsets with the aim of getting familiear with
+scikit-learn. I am following Sebastian Raschka's Python Machine Learning book.
+
+"""
+
